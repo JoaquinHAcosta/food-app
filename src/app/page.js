eec073d8fa2d68c617cb7612e190964235bb8bd0
Joaquin Acosta <joaquinacosta397@outlook.com>
2023-11-22 14:57:36 -0300

    header finished

--- src/app/page.js
+++ src/app/page.js
@@ -1,21 +1,9 @@
-import Link from 'next/link'
+import Header from '@/components/layout/Header'
 
 export default function Home() {
   return (
     <>
-      <header>
-        <Link className="text-primary font-semibold text-2xl" href="">
-          ST PIZZA
-        </Link>
-
-        <nav>
-          <Link href={''}>Home</Link>
-          <Link href={''}>Menu</Link>
-          <Link href={''}>About</Link>
-          <Link href={''}>Contact</Link>
-          <Link href={''}>Login</Link>
-        </nav>
-      </header>
+      <Header />
     </>
   )
 }
