f692f5bab29680c0d950e5b26da680ed3f6d2388
Joaquin Acosta <joaquinacosta397@outlook.com>
2023-11-23 20:23:38 -0300

    about contact & footer

--- src/components/layout/HomeMenu.js
+++ src/components/layout/HomeMenu.js
@@ -1,10 +1,11 @@
 import Image from 'next/image'
 import MenuItem from '../menu/MenuItem'
+import SectionHeaders from './SectionHeaders'
 
 const HomeMenu = () => {
   return (
     <section className="">
-      <div className="absolute h-full left-0 right-0 w-full justify-start">
+      <div className="absolute left-0 right-0 w-full justify-start">
         <div className="absolute left-0 -top-[70px] text-left -z-10">
           <Image src={'/sallad1.png'} alt={'sallad'} width={109} height={189} />
         </div>
@@ -13,10 +14,7 @@ const HomeMenu = () => {
         </div>
       </div>
       <div className="text-center mb-4">
-        <h3 className="uppercase text-gray-500 font-semibold leading-4">
-          Check out
-        </h3>
-        <h2 className="text-primary font-bold text-4xl italic">Menu</h2>
+        <SectionHeaders subHeader={'check out'} mainHeader={'Menu'} />
       </div>
       <div className="grid grid-cols-3 gap-4">
         <MenuItem />
