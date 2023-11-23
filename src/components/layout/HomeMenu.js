03eac94cb6f553fb89f6b3fc14d133d7f5f4b98a
Joaquin Acosta <joaquinacosta397@outlook.com>
2023-11-23 20:08:44 -0300

    menu item done

--- src/components/layout/HomeMenu.js
+++ src/components/layout/HomeMenu.js
@@ -1,8 +1,7 @@
 import Image from 'next/image'
-import React from 'react'
+import MenuItem from '../menu/MenuItem'
 
 const HomeMenu = () => {
-  //<></>
   return (
     <section className="">
       <div className="absolute h-full left-0 right-0 w-full justify-start">
@@ -13,11 +12,19 @@ const HomeMenu = () => {
           <Image src={'/sallad2.png'} alt={'sallad'} width={107} height={195} />
         </div>
       </div>
-      <div className="text-center">
+      <div className="text-center mb-4">
         <h3 className="uppercase text-gray-500 font-semibold leading-4">
           Check out
         </h3>
         <h2 className="text-primary font-bold text-4xl italic">Menu</h2>
+      </div>
+      <div className="grid grid-cols-3 gap-4">
+        <MenuItem />
+        <MenuItem />
+        <MenuItem />
+        <MenuItem />
+        <MenuItem />
+        <MenuItem />
       </div>
     </section>
   )
