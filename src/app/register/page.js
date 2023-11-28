dc12a0743b126321fcbb2f87eafb7f714488953c
Joaquin Acosta <joaquinacosta397@outlook.com>
2023-11-27 23:44:38 -0300

    minimum detail

--- src/app/register/page.js
+++ src/app/register/page.js
@@ -73,6 +73,12 @@ const RegisterPage = () => {
           <Image src={'/google.png'} alt="logo google" width={24} height={24} />
           Login with google
         </button>
+        <div className="text-center my-4 text-gray-500 border-t pt-4">
+          Existing account?{' '}
+          <Link className="underline" href={'/login'}>
+            Login here &raquo;
+          </Link>
+        </div>
       </form>
     </section>
   )
