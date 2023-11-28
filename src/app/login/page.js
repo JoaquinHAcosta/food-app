cbebab7240a92dcf88c27c06a71be0174fd8ec96
Joaquin Acosta <joaquinacosta397@outlook.com>
2023-11-28 12:04:38 -0300

    created login component

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,7 +1,37 @@
-import React from 'react'
+'use client'
+import Image from 'next/image'
+import { useState } from 'react'
 
 const LoginPage = () => {
-  return <div>page</div>
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  return (
+    <section className="mt-8">
+      <h1 className="text-center text-primary text-4xl mb-4">Login</h1>
+      <form className="block max-w-xs mx-auto">
+        <input
+          type="email"
+          placeholder="email"
+          value={email}
+          onChange={(ev) => setEmail(ev.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="password"
+          value={password}
+          onChange={(ev) => setPassword(ev.target.value)}
+        />
+        <button type="submit">Login</button>
+        <div className="my-4 text-center text-gray-500">
+          or login with provider
+        </div>
+        <button className="flex gap-4 justify-center">
+          <Image src={'/google.png'} alt="logo google" width={24} height={24} />
+          Login with google
+        </button>
+      </form>
+    </section>
+  )
 }
 
 export default LoginPage
