ad8e0bc3d647e8202371bf1511693db7184a888d
Joaquin Acosta <joaquinacosta397@outlook.com>
2023-11-28 23:35:19 -0300

    PUT file created

---
+++ src/app/api/profile/route.js
@@ -0,0 +1,16 @@
+import { authOptions } from '@/app/api/auth/[...nextauth]/route'
+import mongoose from 'mongoose'
+import { getServerSession } from 'next-auth'
+
+export async function PUT(req) {
+  mongoose.connect(process.env.MONGO_URL)
+  const data = await req.json()
+  const session = await getServerSession(authOptions)
+  console.log(session)
+
+  if ('name' in data) {
+    //update user name
+  }
+
+  return Response.json(true)
+}
