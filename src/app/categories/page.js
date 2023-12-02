33ceebc25207747caa0e503f0cc905b74ac8c037
Joaquin Acosta <joaquinacosta397@outlook.com>
2023-12-02 15:00:24 -0300

    category input added

--- src/app/categories/page.js
+++ src/app/categories/page.js
@@ -1,9 +1,35 @@
 'use client'
 import UserTabs from '@/components/layout/UserTabs'
 import { useProfile } from '@/components/UseProfile'
+import { useState } from 'react'
+import toast from 'react-hot-toast'
 
 const CategoriesPage = () => {
+  const [newCategoryName, setNewCategoryName] = useState('')
   const { loading: profileLoading, data: profileData } = useProfile()
+
+  const handleNewCategorySubmit = async (ev) => {
+    ev.preventDefault()
+    const creationPromise = new Promise(async (resolve, reject) => {
+      const response = await fetch('/api/categories', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ name: newCategoryName }),
+      })
+      if (response.ok) {
+        resolve()
+      } else {
+        reject()
+      }
+    })
+
+    await toast.promise(creationPromise, {
+      loading: 'Creating your new category...',
+      success: 'Category created!',
+      error: 'Error, sorry...',
+    })
+  }
+
   if (profileLoading) {
     return 'Loading user info...'
   }
@@ -13,9 +39,25 @@ const CategoriesPage = () => {
   }
 
   return (
-    <section className="mt-8 max-w-lg mx-auto">
+    <section className="mt-8 max-w-md mx-auto">
       <UserTabs isAdmin={true} />
-      categories
+      <form className="mt-8" onSubmit={handleNewCategorySubmit}>
+        <div className="flex gap-2 items-end">
+          <div className="grow">
+            <label>New Category name</label>
+            <input
+              type="text"
+              value={newCategoryName}
+              onChange={(ev) => setNewCategoryName(ev.target.value)}
+            />
+          </div>
+          <div className="pb-2">
+            <button className="border border-primary" type="submit">
+              Create
+            </button>
+          </div>
+        </div>
+      </form>
     </section>
   )
 }
