d1bb7fd29150f43555e73367f836b118b2b54a96
Joaquin Acosta <joaquinacosta397@outlook.com>
2023-12-02 21:55:24 -0300

    custom sizes

--- src/components/layout/MenuItemForm.js
+++ src/components/layout/MenuItemForm.js
@@ -6,6 +6,22 @@ const MenuItemForm = ({ onSubmit, menuItem }) => {
   const [name, setName] = useState(menuItem?.name || '')
   const [description, setDescription] = useState(menuItem?.description || '')
   const [basePrice, setBasePrice] = useState(menuItem?.basePrice || '')
+  const [sizes, setSizes] = useState([])
+
+  const addSize = () => {
+    setSizes((oldSizes) => {
+      return [...oldSizes, { name: '', price: 0 }]
+    })
+  }
+
+  const editSize = (ev, index, prop) => {
+    const newValue = ev.target.value
+    setSizes((prevSizes) => {
+      const newSizes = [...prevSizes]
+      newSizes[index][prop] = newValue
+      return newSizes
+    })
+  }
 
   return (
     <form
@@ -38,6 +54,35 @@ const MenuItemForm = ({ onSubmit, menuItem }) => {
             value={basePrice}
             onChange={(ev) => setBasePrice(ev.target.value)}
           />
+          <div className="bg-gray-200 p-2 rounded-md mb-2">
+            <label>Sizes</label>
+            {sizes?.length > 0 &&
+              sizes.map((size, i) => (
+                <div className="flex gap-2" key={i}>
+                  <div>
+                    <label>Size name</label>
+                    <input
+                      type="text"
+                      placeholder="Size name"
+                      value={size.name}
+                      onChange={(ev) => editSize(ev, i, 'name')}
+                    />
+                  </div>
+                  <div>
+                    <label>Extra price</label>
+                    <input
+                      type="text"
+                      placeholder="Extra price"
+                      value={size.value}
+                      onChange={(ev) => editSize(ev, i, 'price')}
+                    />
+                  </div>
+                </div>
+              ))}
+            <button onClick={addSize} type="button" className="bg-white">
+              Add item size
+            </button>
+          </div>
           <button type="submit">Save</button>
         </div>
       </div>
